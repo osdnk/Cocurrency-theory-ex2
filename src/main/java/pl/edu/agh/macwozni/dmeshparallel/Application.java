ff7fd7dd1ea56ce4a4fa27530f013d483da029ca
maciekw <maciekw@SkyFall.MW>
2017-10-26 22:49:27 +0200

    reformat files

---
+++ src/main/java/pl/edu/agh/macwozni/dmeshparallel/Application.java
@@ -0,0 +1,10 @@
+package pl.edu.agh.macwozni.dmeshparallel;
+
+class Application {
+
+    public static void main(String args[]) {
+
+        Executor e = new Executor();
+        e.start();
+    }
+}
