56aa65741c87130dc6d33440eeecdec80e99fd4d
osdnk <micosa97@gmail.com>
2018-11-09 01:47:45 +0100

    Reformat code

--- src/main/java/pl/edu/agh/macwozni/dmeshparallel/Executor.java
+++ src/main/java/pl/edu/agh/macwozni/dmeshparallel/Executor.java
@@ -8,15 +8,16 @@ import pl.edu.agh.macwozni.dmeshparallel.production.PDrawer;
 
 
 public class Executor extends Thread {
-    
+
     private final BlockRunner runner;
-    
-    Executor(BlockRunner _runner){
+
+    Executor(BlockRunner _runner) {
         this.runner = _runner;
     }
 
-    public static final class PE extends AbstractProduction<Vertex> {
+    public static class PE extends AbstractProduction<Vertex> {
         Vertex[][] mRes;
+
         PE(Vertex _obj, PDrawer<Vertex> _drawer, Vertex[][] res) {
             super(_obj, _drawer);
             this.mRes = res;
@@ -24,14 +25,15 @@ public class Executor extends Thread {
 
         @Override
         public Vertex apply(Vertex s) {
-            Vertex t1 = new Vertex(s.x + 1 , s.y);
+            Vertex t1 = new Vertex(s.x + 1, s.y);
             mRes[t1.y][t1.x] = t1;
             return t1;
         }
     }
 
-    public static final class PS extends AbstractProduction<Vertex> {
+    public static class PS extends AbstractProduction<Vertex> {
         Vertex[][] mRes;
+
         PS(Vertex _obj, PDrawer<Vertex> _drawer, Vertex[][] res) {
             super(_obj, _drawer);
             this.mRes = res;
@@ -39,7 +41,7 @@ public class Executor extends Thread {
 
         @Override
         public Vertex apply(Vertex s) {
-            Vertex t1 = new Vertex(s.x , s.y + 1);
+            Vertex t1 = new Vertex(s.x, s.y + 1);
             mRes[t1.y][t1.x] = t1;
             return t1;
         }
@@ -48,29 +50,29 @@ public class Executor extends Thread {
     @Override
     public void run() {
         PDrawer drawer = new GraphDrawer();
-        Vertex s = new Vertex(0,0);
+        Vertex s = new Vertex(0, 0);
         int N = 104;
         Vertex[][] res = new Vertex[N][N];
         res[0][0] = s;
-        for (int n = 1; n <=2 * N - 2; n++) {
+        for (int n = 1; n <= 2 * N - 2; n++) {
             for (int i = 0; i <= N - 1; i++) {
                 int j = n - 1 - i;
-                if (j >= 0 && j < N  - 1) {
+                if (j >= 0 && j < N - 1) {
                     Vertex v = res[i][j];
                     PE p = new PE(v, drawer, res);
                     this.runner.addThread(p);
                 }
             }
             if (n <= N - 1) {
-                Vertex v = res[n-1][0];
+                Vertex v = res[n - 1][0];
                 PS p = new PS(v, drawer, res);
                 this.runner.addThread(p);
             }
             this.runner.startAll();
         }
 
-        for (int i = 0; i< N; i ++) {
-            for (int j = 0; j< N; j ++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
                 if (res[i][j] == null) {
                     // if any of block is null our algorithm is incorrect
                     System.out.println("XXXXXXX");
